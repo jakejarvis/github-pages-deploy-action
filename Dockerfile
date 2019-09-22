9314fca85490595034adc2af8b50cd36d4307f7e
Jake Jarvis <jake@jarv.is>
2019-09-21 22:32:52 -0400

    Switch to plain Debian Slim as base image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10
+FROM debian:stretch-slim
 
 LABEL "com.github.actions.name"="Deploy to GitHub Pages"
 LABEL "com.github.actions.description"="This action will handle the building and deploying process of your project to GitHub Pages."
@@ -8,6 +8,9 @@ LABEL "com.github.actions.color"="orange"
 LABEL "repository"="http://github.com/JamesIves/gh-pages-github-action"
 LABEL "homepage"="http://github.com/JamesIves/gh-pages-gh-action"
 LABEL "maintainer"="James Ives <iam@jamesiv.es>"
+
+RUN apt-get update -qqy && \
+    apt-get install -qqy --no-install-recommends git
 
 ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -40,37 +40,33 @@ then
   PAGES_COMMIT_NAME="${GITHUB_ACTOR}"
 fi
 
-# Installs Git.
-apt-get update && \
-apt-get install -y git && \
-
 # Directs the action to the the Github workspace.
-cd "$GITHUB_WORKSPACE" && \
+cd "$GITHUB_WORKSPACE"
 
 # Configures Git.
-git init && \
-git config --global user.email "${PAGES_COMMIT_EMAIL}" && \
-git config --global user.name "${PAGES_COMMIT_NAME}" && \
+git init
+git config --global user.email "${PAGES_COMMIT_EMAIL}"
+git config --global user.name "${PAGES_COMMIT_NAME}"
 
 ## Initializes the repository path using the access token.
-REPOSITORY_PATH="https://${PAGES_TOKEN}@github.com/${GITHUB_ACTOR}/${PAGES_TARGET_REPO}.git" && \
+REPOSITORY_PATH="https://${PAGES_TOKEN}@github.com/${GITHUB_ACTOR}/${PAGES_TARGET_REPO}.git"
 
 # Checks to see if the remote exists prior to deploying.
 # If the branch doesn't exist it gets created here as an orphan.
 if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$PAGES_TARGET_BRANCH" | wc -l)" -eq 0 ];
 then
   echo "Creating remote branch ${PAGES_TARGET_BRANCH} as it doesn't exist..."
-  git checkout "${PAGES_SOURCE_BRANCH:-master}" && \
-  git checkout --orphan "$PAGES_TARGET_BRANCH" && \
-  git rm -rf . && \
-  touch README.md && \
-  git add README.md && \
-  git commit -m "Initial ${PAGES_TARGET_BRANCH} commit" && \
+  git checkout "${PAGES_SOURCE_BRANCH:-master}"
+  git checkout --orphan "$PAGES_TARGET_BRANCH"
+  git rm -rf .
+  touch README.md
+  git add README.md
+  git commit -m "Initial ${PAGES_TARGET_BRANCH} commit"
   git push "$REPOSITORY_PATH" "$PAGES_TARGET_BRANCH"
 fi
 
 # Checks out the base branch to begin the deploy process.
-git checkout "${PAGES_SOURCE_BRANCH:-master}" && \
+git checkout "${PAGES_SOURCE_BRANCH:-master}"
 
 if [ "$PAGES_CNAME" ]; then
   echo "Generating a CNAME file in in the $PAGES_SOURCE_FOLDER directory..."
@@ -78,9 +74,10 @@ if [ "$PAGES_CNAME" ]; then
 fi
 
 # Commits the data to Github.
-echo "Deploying to GitHub..." && \
-git add -f "$PAGES_SOURCE_FOLDER" && \
+echo "Deploying to GitHub..."
+git add -f "$PAGES_SOURCE_FOLDER"
 
-git commit -m "Deploying to ${PAGES_TARGET_BRANCH} - $(date +"%T")" --quiet && \
-git push "$REPOSITORY_PATH" $(git subtree split --prefix "$FOLDER" "${PAGES_SOURCE_BRANCH:-master}"):"$PAGES_TARGET_BRANCH" --force && \
+git commit -m "Pages deploy from ${GITHUB_ACTOR}/${GITHUB_REPOSITORY}:${PAGES_SOURCE_BRANCH}" --quiet
+git push "$REPOSITORY_PATH" "$PAGES_TARGET_BRANCH" --force
+
 echo "Deployment succesful!"
